10a9e6928d1f8cf565782bc3c064cac9de12c30c
Austin <moreno.austin12@gmail.com>
2022-12-03 10:58:05 +0800

    generate button now generates new image

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -1,12 +1,27 @@
+import memesData from "./memesData"
+import { useState } from 'react'
+
 export default function Meme() {
+
+    const [memeImage, setMemeImage] = useState("")
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const random = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[random].url)
+    }
+
+    
     return (
-        <section className="form">
-            <div className="input-container">
-                <input type="text" placeholder="Top text" />
-                <input type="text" placeholder="Bottom text"/>
-            </div>
-            <button className="button">Get a new meme image 🖼️</button>
-        </section>
-        
+        <div className="main">
+            <section className="form">
+                <div className="input-container">
+                    <input type="text" placeholder="Top text" />
+                    <input type="text" placeholder="Bottom text"/>
+                </div>
+                <button className="button" onClick={getMemeImage}>Get a new meme image 🖼️</button>
+            </section>
+            {<img src={memeImage} />}
+        </div>
     )
 }
